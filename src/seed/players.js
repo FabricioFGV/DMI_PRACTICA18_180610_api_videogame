96a7158c121f869c76e97004694999a9e61e09b7
FabricioFGV <62354571+FabricioFGV@users.noreply.github.com>
2023-11-29 10:34:12 -0600

    seed

---
+++ src/seed/players.js
@@ -0,0 +1,12 @@
+const players = [
+    {
+        name: "Juan Perez",
+        email: "juan.perez@gmail.com",
+        nickname: "1234",
+        password: "JoanPerer",
+        birthdate: "1997-05-02",
+        portrait_img: "default.img",
+    }
+]
+
+export default players
