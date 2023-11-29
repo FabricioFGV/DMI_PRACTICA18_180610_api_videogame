0c2aa8d35dfe7725921aa40644c9630506b03974
FabricioFGV <62354571+FabricioFGV@users.noreply.github.com>
2023-11-29 11:24:08 -0600

    seeder

---
+++ src/seed/games.js
@@ -0,0 +1,21 @@
+const games = [{
+    title: "Game #, level: #",
+    startAt: Date.now(),
+    endsAt: Date.now() + generateRandom(5000,300000),
+    score: generateRandom(0, 100000),
+    player_ID: generateRandom(1,10)
+}]
+
+function generateRandom (min =0, max=100){
+    let difference = max -min;
+
+    let rand =Math.random();
+
+    rand = Math.floor(rand * difference);
+
+    rand = rand +min;
+
+    return rand;
+}
+
+export default games
---
+++ src/seed/seederVideogame.js
@@ -0,0 +1,6 @@
+import {exit} from 'node:process';
+import players from './players';
+import games from './games';
+import {Player, Game} from '../models/relationships.js'
+import db from '../config/db.js'
+
